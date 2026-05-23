bfabe75fc4a7ad07203bef7e523900d9379e5d00
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2026-05-23 01:33:05 -0300

    fix: correct command not found message placement in main function

--- main.go
+++ main.go
@@ -16,12 +16,15 @@ func main() {
 		command = strings.TrimSpace(command)
 		if command == "exit" {
 			break
+		} else if strings.HasPrefix(command, "echo ") {
+			fmt.Println(command[5:])
+		} else {
+			fmt.Println(command + ": command not found")
 		}
 
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input: ", err)
 			os.Exit(1)
 		}
-		fmt.Println(command + ": command not found")
 	}
 }
