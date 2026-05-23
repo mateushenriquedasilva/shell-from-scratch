d2f08618afe59d79044717ce67d5b5e16fefa2c3
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2026-05-23 01:20:50 -0300

    refactor: streamline command input handling in main function

--- main.go
+++ main.go
@@ -4,17 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
-		text, err := reader.ReadString('\n')
+		command, err := reader.ReadString('\n')
+
+		command = strings.TrimSpace(command)
+		if command == "exit" {
+			break
+		}
+
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input: ", err)
 			os.Exit(1)
 		}
-		fmt.Println(text[:len(text)-1] + ": command not found")
+		fmt.Println(command + ": command not found")
 	}
 }
