5a2843d42f7110eaca010e738dd53edfea81313a
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2026-05-26 14:37:41 -0300

    refactor: remove directory change error handling from RunProgram

--- cmd/internal/process/exec.go
+++ cmd/internal/process/exec.go
@@ -1,18 +1,12 @@
 package process
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
 
 func RunProgram(path string, args []string) error {
-	err := os.Chdir(filepath.Dir(path))
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-
 	cmd := exec.Command(filepath.Base(path), args[1:]...)
 	cmd.Dir = filepath.Dir(path)
 
