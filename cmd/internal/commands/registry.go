f2ea58f776b568854248b3491f10333596a48cdc
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2026-05-26 01:18:08 -0300

    feat: add Registry function to map command names to their handlers

--- cmd/internal/commands/registry.go
+++ cmd/internal/commands/registry.go
@@ -1,5 +1,13 @@
 package commands
 
+func Registry() map[string]func(string) {
+	return map[string]func(string){
+		"echo": Echo,
+		"type": Type,
+		"exit": Exit,
+	}
+}
+
 var Builtins = []string{
 	"echo",
 	"type",
