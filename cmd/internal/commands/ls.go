28672a6a716f9f79365f0ae0b12dcea14f9681f4
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2026-05-27 19:59:26 -0300

    refactor: clean up Ls and Pwd functions by removing unused imports and simplifying error handling

--- cmd/internal/commands/ls.go
+++ cmd/internal/commands/ls.go
@@ -1,23 +1,13 @@
 package commands
 
 import (
-	"os"
-
 	"github.com/mateushenriquedasilva/shell-from-scratch/cmd/internal/process"
-	"github.com/mateushenriquedasilva/shell-from-scratch/cmd/internal/utils"
 )
 
 func Ls(command []string) {
 	command = append(command[:1],
 		append([]string{"--color=auto"}, command[1:]...)...,
 	)
-
-	home, err := os.Getwd()
-	utils.Error(err)
-
-	if len(command[1:]) <= 1 {
-		command = append(command, home)
-	}
 
 	process.RunProgram(command[0], command)
 }
--- cmd/internal/commands/pwd.go
+++ cmd/internal/commands/pwd.go
@@ -5,15 +5,13 @@ import (
 	"os"
 
 	"github.com/atotto/clipboard"
+	"github.com/mateushenriquedasilva/shell-from-scratch/cmd/internal/utils"
 )
 
 func Pwd(command []string) {
 	dir, err := os.Getwd()
 	err = clipboard.WriteAll(dir)
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
+	utils.Error(err)
 
 	fmt.Println(dir)
 }
